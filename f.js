d0c6c035555f1ecc650877911084ae73d3bff06a
Mighamaria <47589171+Mighamaria@users.noreply.github.com>
2021-06-27 21:14:22 +0530

    Update f.js

--- f.js
+++ f.js
@@ -6,7 +6,7 @@ let error2 = document.getElementById("error2");
 let error3 = document.getElementById("error3");
 var nameValidation=function()
     {
-      let regexpname= /^([A-Za-z\._]+)$/;
+      let regexpname= /^([A-Za-z]+)$/;
 
       if(regexpname.test(username.value) )
       {
